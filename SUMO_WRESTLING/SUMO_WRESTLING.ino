a1e49036db15b76c4d5701745773a707b8634597
Pranav Putsa <BLENU4CSE18096@bl.students.amrita.edu>
2021-06-04 12:07:42 +0530

    Add files via upload

---
+++ SUMO_WRESTLING/SUMO_WRESTLING.ino
@@ -0,0 +1,48 @@
+
+int motor_one1=9;
+int motor_one2=10;
+int motor_two1=5;
+int motor_two2=3;
+char command;
+void setup() {
+  Serial.begin(9600);// put your setup code here, to run once:
+
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+
+
+
+pinMode(5,OUTPUT);
+pinMode(3,OUTPUT);
+}
+
+void loop() {
+while(Serial.available()>0);
+command=Serial.read();
+if(command=='F')
+{
+analogWrite(9,255);
+ analogWrite(10,0);
+ analogWrite(5,255);
+ analogWrite(3,0);
+}
+else if(command=='L')
+{
+analogWrite(9,0);
+analogWrite(10,0);
+analogWrite(5,255);
+analogWrite(3,0);
+}
+else if(command=='B')
+{
+analogWrite(9,0);
+analogWrite(10,255);
+analogWrite(5,0);
+analogWrite(3,255);
+}else if(command=='R')
+{
+analogWrite(9,0);
+analogWrite(10,255);
+analogWrite(5,0);
+analogWrite(3,0);
+}}
---
+++ SUMO_WRESTLING_2_SWITCHES/SUMO_WRESTLING_2_SWITCHES.ino
@@ -0,0 +1,58 @@
+
+int motor_one1=9;
+int motor_one2=10;
+int motor_two1=5;
+int motor_two2=3;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(6,INPUT_PULLUP);
+
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+
+pinMode(11,INPUT_PULLUP);
+
+pinMode(5,OUTPUT);
+pinMode(3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int sw1=digitalRead(6);
+int sw2=digitalRead(11);
+
+
+
+
+if(sw2==HIGH&&sw1==LOW)
+{analogWrite(9,0);
+ analogWrite(10,0);
+ analogWrite(5,200);
+ analogWrite(3,0);
+ 
+ delay(3000);
+}
+if(sw1==HIGH&&sw2==LOW)
+{
+  analogWrite(9,200);
+ analogWrite(10,0);
+ analogWrite(5,0);
+ analogWrite(3,0);
+ delay(3000);
+}
+if(sw1==HIGH && sw2==HIGH)
+{analogWrite(9,200);
+ analogWrite(10,0);
+ analogWrite(5,200);
+ analogWrite(3,0);
+  delay(3000);
+ 
+}
+else{
+analogWrite(9,100);
+analogWrite(10,0);
+analogWrite(5,100);
+analogWrite(3,0);
+ delay(3000);
+}}
